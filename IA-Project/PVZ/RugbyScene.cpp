69739585e3a414cff7bfc47a2546986c7dfafc33
Riseik <emaillard@gaming.tech>
2025-01-15 12:06:16 +0100

    pass

--- IA-Project/PVZ/RugbyScene.cpp
+++ IA-Project/PVZ/RugbyScene.cpp
@@ -50,6 +50,12 @@ void RugbyScene::OnEvent(const sf::Event& event)
 			playerSelected->GoToPosition(event.mouseButton.x, event.mouseButton.y, 100.f);
 		}
 	}
+
+	if (event.type == event.KeyPressed && event.key.code == sf::Keyboard::P)
+	{
+		Debug::DrawText(100, 100, "PASS", 1.f, 1.f, sf::Color::Red);
+		Pass();
+	}
 }
 
 void RugbyScene::OnUpdate()
@@ -69,11 +75,43 @@ void RugbyScene::OnUpdate()
 		Debug::DrawCircle(position.x, position.y, 10, sf::Color::Blue);
 	}
 
+	for (int i = 0; i < PLAYER_PER_TEAM; i++)
+	{
+		Player** pTeam;
+
+		if (isInTeam(playerWithBall, pTeam1))
+			pTeam = pTeam1;
+		else
+			pTeam = pTeam2;
+
+		if (pTeam[i] == playerWithBall)
+		{
+			passPossible[i] = false;
+			continue;
+		}
+		passPossible[i] = TryPassPossible(pTeam[i]);
+
+		if (passPossible[i])
+			Debug::DrawLine(playerWithBall->GetPosition().x, playerWithBall->GetPosition().y, pTeam[i]->GetPosition().x, pTeam[i]->GetPosition().y, sf::Color::Green);
+		else
+			Debug::DrawLine(playerWithBall->GetPosition().x, playerWithBall->GetPosition().y, pTeam[i]->GetPosition().x, pTeam[i]->GetPosition().y, sf::Color::Red);
+
+	}
+
+	if (playerWithBall->GetPosition().x > width - width / 10 || playerWithBall->GetPosition().x < width / 10)
+	{
+		OnGoal();
+	}
 }
 
-void RugbyScene::OnGoal(Player* player)
+void RugbyScene::OnGoal()
 {
-	if (player = playerWithBall) {
+	if (isInTeam(playerWithBall, pTeam1))
+	{
+		Debug::DrawText(100, 100, "GOAL", 1.f, 1.f, sf::Color::Green);
+	}
+	else
+	{
 		Debug::DrawText(100, 100, "GOAL", 1.f, 1.f, sf::Color::Red);
 	}
 }
@@ -84,4 +122,70 @@ void RugbyScene::TrySetSelectedEntity(Player* player, int x, int y)
 		return;
 
 	playerSelected = player;
+}
+
+float RugbyScene::GetPlayerDistance(Player* p1, Player* p2)
+{
+	int x1 = p1->GetPosition().x;
+	int y1 = p1->GetPosition().y;
+	int x2 = p2->GetPosition().x;
+	int y2 = p2->GetPosition().y;
+
+	float distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+
+	return distance;
+}
+
+Player* RugbyScene::GetClosestPlayer(Player* pTeam[])
+{
+	int indexClosest = 0;
+	float closestDistance = 100000.f;
+
+	for (int i = 0; i < PLAYER_PER_TEAM; i++)
+	{
+		if (pTeam[i] == playerWithBall)
+			continue;
+
+		float distance = GetPlayerDistance(playerWithBall, pTeam[i]);
+
+		if (distance < closestDistance)
+		{
+			indexClosest = i;
+			closestDistance = distance;
+		}
+	}
+
+	return pTeam[indexClosest];
+}
+
+bool RugbyScene::isInTeam(Player* player, Player* pTeam[])
+{
+	for (int i = 0; i < PLAYER_PER_TEAM; i++)
+	{
+		if (player == pTeam[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool RugbyScene::TryPassPossible(Player* player)
+{
+	if (sqrt(GetPlayerDistance(playerWithBall, player)) > 200)
+		return false;
+
+	if (isInTeam(playerWithBall, pTeam1) && playerWithBall->GetPosition().x < player->GetPosition().x)
+		return false;
+
+	if (isInTeam(playerWithBall, pTeam2) && playerWithBall->GetPosition().x > player->GetPosition().x)
+		return false;
+
+	return true;
+}
+
+void RugbyScene::Pass()
+{
+	Player* closestPlayer = GetClosestPlayer(pTeam1);
+	playerWithBall = closestPlayer;
 }
--- IA-Project/PVZ/RugbyScene.h
+++ IA-Project/PVZ/RugbyScene.h
@@ -28,14 +28,22 @@ public:
 	Player* pTeam1[PLAYER_PER_TEAM];
 	Player* pTeam2[PLAYER_PER_TEAM];
 
+	bool passPossible[5] = {false, false, false, false, false};
+
 private:
 	void TrySetSelectedEntity(Player* player, int x, int y);
+	float GetPlayerDistance(Player* p1, Player* p2);
+	Player* GetClosestPlayer(Player* pTeam[]);
+	bool isInTeam(Player* player, Player* pTeam[]);
+
+	bool TryPassPossible(Player* player);
+	void Pass();
 
 public:
 	void OnInitialize() override;
 	void OnEvent(const sf::Event& event) override;
 	void OnUpdate() override;
 
-	void OnGoal(Player* player);
+	void OnGoal();
 };
 
