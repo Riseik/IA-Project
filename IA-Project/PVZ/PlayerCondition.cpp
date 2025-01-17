dde3ef3ab252d847c00558920dbc273ed0349d97
joachim-gabin <joachimgabin@gmail.com>
2025-01-17 13:56:07 +0100

    Update PlayerCondition.cpp

--- IA-Project/PVZ/PlayerCondition.cpp
+++ IA-Project/PVZ/PlayerCondition.cpp
@@ -85,10 +85,10 @@ bool PlayerCondition_BallPlayerBehind::OnTest(Player* owner)
 
 	if (team == scene->GetPlayerTeam(scene->playerWithBall))
 	{
-		if (team == scene->pTeam1 && scene->playerWithBall->GetPosition().x - 50.f < owner->GetPosition().x)
+		if (team == scene->pTeam1 && scene->playerWithBall->GetPosition().x - 100.f < owner->GetPosition().x)
 			return true;
 
-		else if (team == scene->pTeam2 && scene->playerWithBall->GetPosition().x + 50.f > owner->GetPosition().x)
+		else if (team == scene->pTeam2 && scene->playerWithBall->GetPosition().x + 100.f > owner->GetPosition().x)
 			return true;
 	}
 
